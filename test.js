8594cf3e30f8975294fcd3277d2dc1875bfda431
zacisyourmaster <zac15590@gmail.com>
2025-02-23 13:14:30 -0800

    added mainHandler.js We can probably do all logic in one file

---
+++ test.js
@@ -0,0 +1,15 @@
+const { handler } = require("./backend/functions/mainHandler");
+const fs = require("fs");
+const imageBase64 = fs.readFileSync("test.png", "base64");
+console.log(imageBase64);
+const event = {
+    body: JSON.stringify({
+        image: imageBase64,
+    }),
+};
+
+const context = {};
+
+handler(event, context)
+    .then((response) => console.log("Response:", response))
+    .catch((error) => console.error("Error:", error));
